a0435a7bf32deb1f71ca0585e88b523c064bd8d4
Aayush Anand <aayushanand27@gmail.com>
2024-08-07 18:22:13 +0530

    Add files via upload

---
+++ reverse_word.java
@@ -0,0 +1,22 @@
+public class reverse_word {
+    public static void main(String[] args) {
+        String str= "Hi , I Am Aayush Anand";
+        StringBuilder sb = new StringBuilder("");
+        String ans = "";
+        for(int i=0; i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch != ' '){
+                sb.append(ch);
+            }
+            else{
+                sb.reverse();
+                ans += sb;
+                ans += ' ';
+                sb= new StringBuilder("");
+            }
+        }
+        sb.reverse();
+        ans += sb;
+        System.out.println(ans);
+    }
+}
